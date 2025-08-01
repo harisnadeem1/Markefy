96693926d7b23993006e3a63ec9a8b93b7f6d09b
Muhammad Haris Nadeem <bscs21062@itu.edu.pk>
2025-08-02 03:40:53 +0500

    jg

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -42,7 +42,6 @@ const ResultsPage = () => {
                     </p>
                 </motion.div>
 
-                <TangibleResults />
                 
                 <div className="py-16 md:py-24 bg-gray-50">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-20">
